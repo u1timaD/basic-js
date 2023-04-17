28b237700a26fe3ec1446aeb66f89aaf30c80f47
Dmitry <free-doom46@mail.ru>
2023-04-17 18:36:46 +0300

    feat: finish carbon-dating and dream-team task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string" || isNaN(parseFloat(sampleActivity))) {
+    return false;
+  }
+  
+  const sampleActivityFloat = parseFloat(sampleActivity);
+  if (sampleActivityFloat <= 0 || sampleActivityFloat >= MODERN_ACTIVITY) {
+    return false;
+  }
+  
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const age = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivityFloat) / k);
+  return age;
 }
+
 
 module.exports = {
   dateSample
